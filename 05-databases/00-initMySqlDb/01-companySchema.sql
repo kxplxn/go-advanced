8e0e7eab4827693ef8fe6dc968e7a433b02769e0
Alican Demirtas <alcadeta@icloud.com>
2021-12-04 15:04:55 +0000

    020500 Databases: Init

---
+++ 05-databases/00-initMySqlDb/01-companySchema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `company`;
+
+USE `company`;
+
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20),
+  `city` VARCHAR(20) DEFAULT "fredericton",
+  `dept` VARCHAR(20) DEFAULT "onboarding",
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4;
